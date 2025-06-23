89745b08ca0092ef8a1e077a33a7b54912d4a241
dsds60321 <dsds60321@gmail.com>
2025-06-24 00:09:23 +0900

    first commit

---
+++ src/test/java/dev/gunho/api/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gunho.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
